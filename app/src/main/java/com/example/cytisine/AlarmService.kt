14c8f3789539829e2cf9d4ec26647fada9b95e95
Artyom Shutoff <37169190+artyomshutoff@users.noreply.github.com>
2026-08-17 10:14:48 +0300

    Add files via upload

--- app/src/main/java/com/example/cytisine/AlarmService.kt
+++ app/src/main/java/com/example/cytisine/AlarmService.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.media.AudioAttributes
 import android.media.MediaPlayer
+import android.media.RingtoneManager
 import android.net.Uri
 import android.os.Build
 import android.os.IBinder
